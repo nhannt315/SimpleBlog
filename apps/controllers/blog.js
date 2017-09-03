f87ad7a3abc5e0d6eda18af1c83b9d2065b7e417
NhanNT <nhannt.bk@gmail.com>
2017-09-03 23:22:57 +0700

    Create blog home page,post detail and about page

--- apps/controllers/blog.js
+++ apps/controllers/blog.js
@@ -1,10 +1,41 @@
 var express = require("express");
 var router = express.Router();
+var post_md = require("../models/post");
 
-router.get("/", function(req, res) {
-    res.json({
-        "message": "This is blog page"
-    })
+router.get("/", function (req, res) {
+    var data = post_md.getAllPosts();
+    data.then(function (posts) {
+        var result = {
+            posts: posts,
+            error: false
+        };
+        res.render("blog/index", {data: result});
+    }).catch(function (error) {
+        var result = {
+            error: "Could not get all posts"
+        };
+        res.render("blog/index", {data: result});
+    });
 });
 
+router.get("/post/:id", function (req, res) {
+   var data = post_md.getPostById(req.params.id);
+   data.then(function (posts) {
+      var post = posts[0];
+      var result = {
+          post: post,
+          error: false
+      };
+      res.render("blog/post", {data: result});
+   }).catch(function (error) {
+       var result = {
+           error: "Cannot find post"
+       };
+       res.render("blog/post", {data: result});
+   });
+});
+
+router.get("/about", function (req, res) {
+   res.render("blog/about");
+});
 module.exports = router;
